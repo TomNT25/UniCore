ba51f25d75e5c8b6bd43e3998dda722509f7d1f8
Tung Nguyen Thanh <TungNguyenThanh25@users.noreply.github.com>
2026-09-24 09:10:47 +0700

    modify

--- UniCore.Backend/UniCore.Application/Feature/v1/Auth/Login/LoginResponseDTO.cs
+++ UniCore.Backend/UniCore.Application/Feature/v1/Auth/Login/LoginResponseDTO.cs
@@ -5,8 +5,6 @@ namespace UniCore.Application.Feature.v1.Auth.Login
     public class LoginResponseDTO
     {
         public string AccessToken { get; set; } = string.Empty;
-        public string RefreshToken { get; set; } = string.Empty;
-        public int RefreshTokenExpire { get; set; }
         public UserLoginResponseDTO User { get; set; } = default!;
     }
 
@@ -18,6 +16,5 @@ namespace UniCore.Application.Feature.v1.Auth.Login
         public string Email { get; set; } = string.Empty;
 
         public List<RoleDTO> Roles { get; set; } = new List<RoleDTO>();
-        public List<PermissionDTO> Permissions { get; set; } = new List<PermissionDTO>();
     }
 }
--- UniCore.Backend/UniCore.Application/Feature/v1/Auth/RefreshToken/RefreshTokenResponseDTO.cs
+++ UniCore.Backend/UniCore.Application/Feature/v1/Auth/RefreshToken/RefreshTokenResponseDTO.cs
@@ -3,7 +3,5 @@ namespace UniCore.Application.Feature.v1.Auth.RefreshToken
     public class RefreshTokenResponseDTO
     {
         public string AccessToken { get; set; } = string.Empty;
-        public string RefreshToken { get; set; } = string.Empty;
-        public int ExpiresIn { get; set; }
     }
 }
